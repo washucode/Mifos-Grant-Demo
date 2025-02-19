730037e9d837496bb57c40b791cb60bdc557c18a
Esther Wachuka <estherw8525@gmail.com>
2025-02-19 21:28:30 +0400

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
   fineractPlatformTenantIds: loadedEnv['fineractPlatformTenantIds'] || 'default',
   // For connecting to others servers running elsewhere update the base API URL
   baseApiUrls:
-    loadedEnv['fineractApiUrls'] ||
+    loadedEnv['FINERACT_API_URLS'] ||
     'https://dev.mifos.io,https://demo.mifos.io,https://qa.mifos.io,https://staging.mifos.io,https://mobile.mifos.io,https://demo.fineract.dev,https://localhost:8443,' +
       window.location.protocol +
       '//' +
